a44fd36c111d0f7d72681fbd7cc7e80817353132
zainulrauf <54389092+zainulrauf@users.noreply.github.com>
2023-08-15 20:18:17 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" href="{{ asset('img/inxgo favicon.ico') }}" type="image/x-icon">
